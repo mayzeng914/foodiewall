53601206ed4bd99aa63a13106e88c147b039fd4a
MavisZeng <mayzeng914@hotmail.com>
2014-08-08 17:08:41 -0700

    before setting up personal page

--- foodiewall/app/controllers/foodiepictures_controller.rb
+++ foodiewall/app/controllers/foodiepictures_controller.rb
@@ -21,22 +21,33 @@ class FoodiepicturesController < ApplicationController
 			redirect_to new_session_path
 			return
 		end
-		@foodiepicture = Foodiepicture.new(params.require(:foodiepicture).permit(:image, :description) )
-		if @foodiepicture.save
+		foodiepicture = current_user.foodiepictures.new(params.require(:foodiepicture).permit(:image, :description) )
+		if foodiepicture.save
 			redirect_to foodiepictures_path
 		end
 	end
 
 	def edit
 		@foodiepicture = Foodiepicture.find(params[:id])
+		if (!current_user) || (@foodiepicture.user != current_user)
+			redirect_to new_session_path
+			return
+		end
 	end
 
 	def update
 		@foodiepicture = Foodiepicture.find(params[:id])
-		if @foodiepicture.update_attributes(params.require(:foodiepicture).permit(:image, :description) )
-			redirect_to foodiepictures_path
+		if (!current_user) || (@foodiepicture.user)
+			redirect_to new_session_path
+			return
+		elsif (@foodiepicture.user == current_user)
+			if @foodiepicture.update_attributes(params.require(:foodiepicture).permit(:image, :description) )
+				redirect_to foodiepictures_path
+			else
+				render 'edit'
+			end
 		else
-			render 'edit'
+			redirect_to new_session_path
 		end
 	end
 
