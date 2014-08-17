fa3a51f32daa3ff10e2caaabfacf38a321832a6e
MavisZeng <mayzeng914@hotmail.com>
2014-08-17 12:30:48 -0700

    user page works again

--- foodiewall/app/controllers/users_controller.rb
+++ foodiewall/app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:user_id])
+  	@user = User.find(params[:id])
     @foodiepictures = @user.foodiepictures
   end
 
